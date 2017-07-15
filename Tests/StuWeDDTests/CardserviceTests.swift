fe826f298727a194c3561fa5edc9e6b12253b7a2
Kilian Koeltzsch <me@kilian.io>
2017-07-16 00:44:25 +0200

    add login test for 599 status code w/o login
    seems to work the other way around as well :P

---
+++ Tests/StuWeDDTests/CardserviceTests.swift
@@ -0,0 +1,39 @@
+//
+//  CardserviceTests.swift
+//  StuWeDD
+//
+//  Created by Kilian Költzsch on 15.07.17.
+//  Copyright © 2017 StuWeDD. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import StuWeDD
+
+class CardserviceTests: XCTestCase {
+    func testLogin() {
+        let e = expectation(description: "Authenticate unsuccessfully with weird status code")
+        Cardservice.login(username: "", password: "") { result in
+            switch result {
+            case .success(_):
+                XCTFail("Shouldn't succeed with no login details")
+            case .failure(let error):
+                guard
+                    let err = error as? StuWeDD.Error,
+                    case .server(statusCode: let status) = err,
+                    status == 599
+                else {
+                    XCTFail("Failed with unknown error: \(error)")
+                    return
+                }
+                e.fulfill()
+            }
+        }
+
+        waitForExpectations(timeout: 5)
+    }
+
+    static var allTests = [
+        ("testLogin", testLogin)
+    ]
+}
