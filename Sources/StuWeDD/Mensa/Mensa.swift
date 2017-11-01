3b3b34901b52757a214061ed5290c279ad2f053e
Kilian Koeltzsch <me@kilian.io>
2017-11-01 10:29:14 +0100

    refactor Mensa to make id lookup more comfortable

--- Sources/StuWeDD/Mensa/Mensa.swift
+++ Sources/StuWeDD/Mensa/Mensa.swift
@@ -1,82 +1,72 @@
 import Foundation
 
-public enum Mensa {
-    public static let alteMensa = "Alte Mensa"
-    public static let bruehl = "Mensa Brühl"
-    public static let grillCube = "Grill Cube"
-    public static let goerlitz = "Mensa Görlitz"
-    public static let johannstadt = "Mensa Johannstadt"
-    public static let kindertagesstaetten = "Kindertagesstätten"
-    public static let kreuzgymnasium = "Mensa Kreuzgymnasium"
-    public static let mensologie = "Mensologie"
-    public static let paluccaHochschule = "Mensa Palucca Hochschule"
-    public static let reichenbachstrasse = "Mensa Reichenbachstraße"
-    public static let siedepunkt = "Mensa Siedepunkt"
-    public static let sport = "Mensa Sport"
-    public static let stimmGabel = "Mensa Stimm-Gabel"
-    public static let tellerRandt = "Mensa TellerRandt"
-    public static let uBoot = "BioMensa U-Boot"
-    public static let wuEins = "Mensa WUeins"
-    public static let zeltschloesschen = "Zeltschlösschen"
-    public static let zittau = "Mensa Zittau"
+public struct Mensa {
+    public let name: String
+    public let id: Int?
+
+    public static let alteMensa = Mensa(name: "Alte Mensa", id: 18)
+    public static let bruehl = Mensa(name: "Mensa Brühl", id: 34)
+    public static let grillCube = Mensa(name: "Grill Cube", id: 46)
+    public static let goerlitz = Mensa(name: "Mensa Görlitz", id: 15)
+    public static let johannstadt = Mensa(name: "Mensa Johannstadt", id: 32)
+    public static let kindertagesstaetten = Mensa(name: "Kindertagesstätten", id: 21)
+    public static let kreuzgymnasium = Mensa(name: "Mensa Kreuzgymnasium", id: 20)
+    public static let mensologie = Mensa(name: "Mensologie", id: 5)
+    public static let paluccaHochschule = Mensa(name: "Mensa Palucca Hochschule", id: 14)
+    public static let reichenbachstrasse = Mensa(name: "Mensa Reichenbachstraße", id: 9)
+    public static let siedepunkt = Mensa(name: "Mensa Siedepunkt", id: 6)
+    public static let sport = Mensa(name: "Mensa Sport", id: 19)
+    public static let stimmGabel = Mensa(name: "Mensa Stimm-Gabel", id: 13)
+    public static let tellerRandt = Mensa(name: "Mensa TellerRandt", id: 7)
+    public static let uBoot = Mensa(name: "BioMensa U-Boot", id: 22)
+    public static let wuEins = Mensa(name: "Mensa WUeins", id: 33)
+    public static let zeltschloesschen = Mensa(name: "Zeltschlösschen", id: 35)
+    public static let zittau = Mensa(name: "Mensa Zittau", id: 1)
+
+    public static let all = [alteMensa, bruehl, grillCube, goerlitz, johannstadt, kindertagesstaetten, kreuzgymnasium, mensologie, paluccaHochschule, reichenbachstrasse, siedepunkt, sport, stimmGabel, tellerRandt, uBoot, wuEins, zeltschloesschen, zittau]
 
     static func id(for mensa: String) -> Int? {
-        switch mensa {
-        case Mensa.alteMensa: return 18
-        case Mensa.bruehl: return 34
-        case Mensa.grillCube: return 46
-        case Mensa.goerlitz: return 15
-        case Mensa.johannstadt: return 32
-        case Mensa.kindertagesstaetten: return 21
-        case Mensa.kreuzgymnasium: return 20
-        case Mensa.mensologie: return 5
-        case Mensa.paluccaHochschule: return 14
-        case Mensa.reichenbachstrasse: return 9
-        case Mensa.siedepunkt: return 6
-        case Mensa.sport: return 19
-        case Mensa.stimmGabel: return 13
-        case Mensa.tellerRandt: return 7
-        case Mensa.uBoot: return 22
-        case Mensa.wuEins: return 33
-        case Mensa.zeltschloesschen: return 35
-        case Mensa.zittau: return 1
-        default:
-            print("No ID known for '\(mensa)'.")
-            return nil
-        }
+        return all
+            .filter { $0.name.lowercased() == mensa.lowercased() }
+            .first?
+            .id
     }
 
     /// Normalize common aliases.
-    public static func from(name: String) -> String {
+    public static func from(name: String) -> Mensa {
+        if let mensa = all.filter({ $0.name.lowercased() == name.lowercased() }).first {
+            return mensa
+        }
+
         switch name.lowercased() {
         case "alte", "mensa", "mommsa", "brat2", "bratquadrat":
-            return Mensa.alteMensa
+            return alteMensa
         case "uboot", "bio", "biomensa":
-            return Mensa.uBoot
-        case "brühl", "hfbk":
-            return Mensa.bruehl
-        case "görlitz":
-            return Mensa.goerlitz
+            return uBoot
+        case "brühl", "bruehl", "hfbk":
+            return bruehl
+        case "görlitz", "goerlitz":
+            return goerlitz
         case "jotown", "ba", "alte fakultät":
-            return Mensa.johannstadt
+            return johannstadt
         case "palucca", "tanz":
-            return Mensa.paluccaHochschule
+            return paluccaHochschule
         case "reichenbach", "htw", "reiche", "club mensa":
-            return Mensa.reichenbachstrasse
+            return reichenbachstrasse
         case "siede", "siedepunkt", "drepunct", "drehpunkt", "siedepunct", "slub":
-            return Mensa.siedepunkt
+            return siedepunkt
         case "stimmgabel", "hfm", "musik", "musikhochschule":
-            return Mensa.stimmGabel
+            return stimmGabel
         case "tharandt", "tellerrand":
-            return Mensa.tellerRandt
+            return tellerRandt
         case "wu", "wu1", "wundtstraße":
-            return Mensa.wuEins
+            return wuEins
         case "zelt", "zeltmensa", "schlösschen", "feldschlösschen", "neue":
-            return Mensa.zeltschloesschen
-        case "grill", "cube", "fresswürfel", "würfel", "burger", "grillcube":
-            return Mensa.grillCube
+            return zeltschloesschen
+        case "grill", "cube", "fresswürfel", "würfel", "wuerfel", "burger", "grillcube":
+            return grillCube
         default:
-            return name
+            return Mensa(name: name, id: nil)
         }
     }
 }
