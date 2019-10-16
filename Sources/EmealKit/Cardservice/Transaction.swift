1457c8e0a879d6eccbb458cdb74583f729a901e2
Kilian Koeltzsch <me@kilian.io>
2019-10-16 19:11:16 +0200

    Add inits for Transaction and Position

--- Sources/EmealKit/Cardservice/Transaction.swift
+++ Sources/EmealKit/Cardservice/Transaction.swift
@@ -37,6 +37,18 @@ public struct Transaction: Hashable {
         self.positions = positions
     }
 
+    public init(clientID: Int, id: Int, transactionID: String, date: Date, location: String, register: String, kind: Transaction.Kind, amount: Double, positions: [Transaction.Position]) {
+        self.clientID = clientID
+        self.id = id
+        self.transactionID = transactionID
+        self.date = date
+        self.location = location
+        self.register = register
+        self.kind = kind
+        self.amount = amount
+        self.positions = positions
+    }
+
     static func create(from services: [TransactionService], filtering positions: [Position]) throws -> [Transaction] {
         var positionCount = 0
 
@@ -76,8 +88,8 @@ struct TransactionService: Codable {
     }
 }
 
-public extension Transaction {
-    struct Position: Codable, Hashable {
+extension Transaction {
+    public struct Position: Codable, Hashable {
         public let clientID: Int
         public let id: Int
         public let transactionID: String
@@ -87,6 +99,18 @@ public extension Transaction {
         public let price: Double
         public let totalPrice: Double
         public let rating: Int // ?
+
+        public init(clientID: Int, id: Int, transactionID: String, positionID: Int, name: String, amount: Int, price: Double, totalPrice: Double, rating: Int) {
+            self.clientID = clientID
+            self.id = id
+            self.transactionID = transactionID
+            self.positionID = positionID
+            self.name = name
+            self.amount = amount
+            self.price = price
+            self.totalPrice = totalPrice
+            self.rating = rating
+        }
 
         private enum CodingKeys: String, CodingKey {
             case clientID = "mandantId"
