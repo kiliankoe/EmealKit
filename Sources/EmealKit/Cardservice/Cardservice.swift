47261d97361e240c09e6a67d59267e55d2e307cd
Kilian Koeltzsch <me@kilian.io>
2020-03-02 20:38:29 +0100

    import FoundationNetworking if available
    necessary on Linux

--- Sources/EmealKit/Cardservice/Cardservice.swift
+++ Sources/EmealKit/Cardservice/Cardservice.swift
@@ -7,6 +7,9 @@
 //
 
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 public struct Cardservice {
     let username: String
--- Sources/EmealKit/Network.swift
+++ Sources/EmealKit/Network.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
 
 extension URL {
     static var cardservice: URL {
