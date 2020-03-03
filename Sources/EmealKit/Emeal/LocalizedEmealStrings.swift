e20b02af38402f06cf4f1b8323693aca0193e6da
Kilian Koeltzsch <me@kilian.io>
2020-03-03 10:29:08 +0100

    Add public initializer for localized strings

--- Sources/EmealKit/Emeal/LocalizedEmealStrings.swift
+++ Sources/EmealKit/Emeal/LocalizedEmealStrings.swift
@@ -1,5 +1,11 @@
-public struct LocalizedEmealStrings {
+public class LocalizedEmealStrings {
     let alertMessage: String
     let nfcReadingError: String
     let nfcConnectionError: String
+
+    public init(alertMessage: String, nfcReadingError: String, nfcConnectionError: String) {
+        self.alertMessage = alertMessage
+        self.nfcReadingError = nfcReadingError
+        self.nfcConnectionError = nfcConnectionError
+    }
 }
