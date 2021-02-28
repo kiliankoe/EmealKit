34daf8e6a148e674004f77c84445800d77d37023
Kilian Koeltzsch <me@kilian.io>
2021-02-28 18:56:05 +0100

    use AnyObject instead of class protocol
    the latter is deprecated

--- Sources/EmealKit/Emeal/EmealDelegate.swift
+++ Sources/EmealKit/Emeal/EmealDelegate.swift
@@ -1,4 +1,4 @@
-public protocol EmealDelegate: class {
+public protocol EmealDelegate: AnyObject {
     func invalidate(with error: Error)
     func readData(currentBalance: Double, lastTransaction: Double)
 }
