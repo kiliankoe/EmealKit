7c11d3f327a30fc1f8ad61de37aa0f57d6bcafc9
Kilian Koeltzsch <me@kilian.io>
2021-12-07 13:11:42 +0100

    Clean up Cardservice tests

--- Tests/EmealKitTests/CardserviceTests.swift
+++ Tests/EmealKitTests/CardserviceTests.swift
@@ -4,31 +4,14 @@ import EmealKit
 
 @available(macOS 12.0, iOS 15.0, *)
 class CardserviceTests: XCTestCase {
-    var cardservice: Cardservice?
+    lazy var username: String = ProcessInfo.processInfo.environment["EMEAL_USERNAME"]!
+    lazy var password: String = ProcessInfo.processInfo.environment["EMEAL_PASSWORD"]!
 
-    override func setUp() async throws {
-        try await super.setUp()
-
-        guard self.cardservice == nil else {
-            // Let's skip subsequent login calls so as not to spam the API.
-            return
-        }
-
-        guard let username = ProcessInfo.processInfo.environment["EMEAL_USERNAME"],
-              let password = ProcessInfo.processInfo.environment["EMEAL_PASSWORD"]
-        else {
-            XCTFail("No authentication details found in environment.")
-            return
-        }
-        do {
-            self.cardservice = try await Cardservice.login(username: username, password: password)
-        } catch {
-            XCTFail("Unexpected error: \(error)")
-        }
-    }
+    // The API is quick to throw 429 status codes on too many consecutive requests, that's why we're sleeping half a
+    // second between them.
 
     func testLoginFailure() async {
-        Thread.sleep(forTimeInterval: 1)
+        Thread.sleep(forTimeInterval: 0.5)
         do {
             _ = try await Cardservice.login(username: "", password: "")
             XCTFail("Shouldn't succeed with no login details")
@@ -40,29 +23,35 @@ class CardserviceTests: XCTestCase {
         }
     }
 
-    func _testLoginSuccess() async {
-        // No need to test this, since other tests implicitly test successful authentication as well.
+    func testLoginSuccess() async {
+        Thread.sleep(forTimeInterval: 0.5)
+        do {
+            _ = try await Cardservice.login(username: username, password: password)
+        } catch {
+            XCTFail("Login should not fail. \(error)")
+        }
     }
 
     func testFetchCarddata() async {
-        Thread.sleep(forTimeInterval: 1)
+        Thread.sleep(forTimeInterval: 0.5)
         do {
-            let carddata = try await self.cardservice?.carddata()
-            XCTAssertNotNil(carddata)
+            let cardservice = try await Cardservice.login(username: username, password: password)
+            let carddata = try await cardservice.carddata()
+            XCTAssert(!carddata.isEmpty)
         } catch {
-            XCTFail("Unexpected error: \(error)")
+            XCTFail("Fetching carddata should not fail. \(error)")
         }
     }
 
     func testFetchTransactions() async {
-        Thread.sleep(forTimeInterval: 1)
+        Thread.sleep(forTimeInterval: 0.5)
         do {
-            let lastWeek = Date().addingTimeInterval(-1 * 60 * 60 * 24 * 7)
-            let today = Date()
-            let transactions = try await self.cardservice?.transactions(begin: lastWeek, end: today)
-            XCTAssertNotNil(transactions)
+            let cardservice = try await Cardservice.login(username: username, password: password)
+            let oneWeekAgo = Date().addingTimeInterval(-1 * 60 * 60 * 24 * 7)
+            _ = try await cardservice.transactions(begin: oneWeekAgo)
+            // Can't really assert anything here, the list of transactions is likely empty, but at least it didn't fail.
         } catch {
-            XCTFail("Unexpected error: \(error)")
+            XCTFail("Fetching transactions should not fail. \(error)")
         }
     }
 }
