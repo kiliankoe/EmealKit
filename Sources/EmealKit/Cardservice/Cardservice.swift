f9ef68e85fd810ad39daef5fb2cd9cd64e486860
Kilian Koeltzsch <me@kilian.io>
2021-12-07 12:56:26 +0100

    Add default end date param and reformatting

--- Sources/EmealKit/Cardservice/Cardservice.swift
+++ Sources/EmealKit/Cardservice/Cardservice.swift
@@ -20,8 +20,14 @@ public struct Cardservice {
     ///   - password: Your password
     ///   - session: URLSession, defaults to .shared
     ///   - completion: handler
-    public static func login(username: String, password: String, session: URLSession = .shared, completion: @escaping (Result<Cardservice, CardserviceError>) -> Void) {
-        guard let url = URL(string: "?karteNr=\(username)&format=JSON&datenformat=JSON", relativeTo: URL.Cardservice.login) else {
+    public static func login(
+        username: String,
+        password: String,
+        session: URLSession = .shared,
+        completion: @escaping (Result<Cardservice, CardserviceError>) -> Void
+    ) {
+        guard let url = URL(string: "?karteNr=\(username)&format=JSON&datenformat=JSON",
+                            relativeTo: URL.Cardservice.login) else {
             completion(.failure(.invalidURL))
             return
         }
@@ -36,7 +42,8 @@ public struct Cardservice {
         """.data(using: .utf8)
         request.httpBody = data
 
-        session.cardserviceDataTask(with: request, session: session) { (result: Result<[LoginResponse], CardserviceError>) in
+        session.cardserviceDataTask(with: request, session: session) {
+            (result: Result<[LoginResponse], CardserviceError>) in
             switch result {
             case .failure(let error):
                 completion(.failure(error))
@@ -58,7 +65,11 @@ public struct Cardservice {
     ///   - session: URLSession, defaults to .shared
     /// - Returns: An authenticated `Cardservice`.
     @available(macOS 12.0, iOS 15.0, *)
-    public static func login(username: String, password: String, session: URLSession = .shared) async throws -> Cardservice {
+    public static func login(
+        username: String,
+        password: String,
+        session: URLSession = .shared
+    ) async throws -> Cardservice {
         try await withCheckedThrowingContinuation { continuation in
             self.login(username: username, password: password, session: session) { result in
                 continuation.resume(with: result)
@@ -71,13 +82,18 @@ public struct Cardservice {
     /// - Parameters:
     ///   - session: URLSession, defaults to .shared
     ///   - completion: handler
-    public func carddata(session: URLSession = .shared, completion: @escaping (Result<[CardData], CardserviceError>) -> Void) {
-        guard let url = URL(string: "?format=JSON&authToken=\(self.authToken)&karteNr=\(self.cardnumber)", relativeTo: URL.Cardservice.carddata) else {
+    public func carddata(
+        session: URLSession = .shared,
+        completion: @escaping (Result<[CardData], CardserviceError>) -> Void
+    ) {
+        guard let url = URL(string: "?format=JSON&authToken=\(self.authToken)&karteNr=\(self.cardnumber)",
+                            relativeTo: URL.Cardservice.carddata) else {
             completion(.failure(.invalidURL))
             return
         }
         let request = URLRequest(url: url)
-        session.cardserviceDataTask(with: request, session: session) { (result: Result<[CardDataService], CardserviceError>) in
+        session.cardserviceDataTask(with: request, session: session) {
+            (result: Result<[CardDataService], CardserviceError>) in
             switch result {
             case .failure(let error):
                 completion(.failure(error))
@@ -107,7 +123,12 @@ public struct Cardservice {
     ///   - end: end of time interval
     ///   - session: URLSession, defaults to .shared
     ///   - completion: handler
-    public func transactions(begin: Date, end: Date, session: URLSession = .shared, completion: @escaping (Result<[Transaction], CardserviceError>) -> Void) {
+    public func transactions(
+        begin: Date,
+        end: Date = Date(),
+        session: URLSession = .shared,
+        completion: @escaping (Result<[Transaction], CardserviceError>) -> Void
+    ) {
         guard
             let transactionURL = URL(
                 string: "?format=JSON&authToken=\(self.authToken)&karteNr=\(self.cardnumber)&datumVon=\(begin.dayMonthYear)&datumBis=\(end.dayMonthYear)",
@@ -125,9 +146,12 @@ public struct Cardservice {
         let transactionRequest = URLRequest(url: transactionURL)
         let positionsRequest = URLRequest(url: positionsURL)
 
-        // TODO: Both requests here should fire simultaneously and be synchronized afterwards. They don't depend on each other.
-        session.cardserviceDataTask(with: transactionRequest, session: session) { (transactionsResult: Result<[TransactionService], CardserviceError>) in
-            session.cardserviceDataTask(with: positionsRequest, session: session) { (positionsResult: Result<[Transaction.Position], CardserviceError>) in
+        // TODO: Both requests here should fire simultaneously and be synchronized afterwards. They don't depend on each
+        // other.
+        session.cardserviceDataTask(with: transactionRequest, session: session) {
+            (transactionsResult: Result<[TransactionService], CardserviceError>) in
+            session.cardserviceDataTask(with: positionsRequest, session: session) {
+                (positionsResult: Result<[Transaction.Position], CardserviceError>) in
                 switch (transactionsResult, positionsResult) {
                 case (.failure(let error), _):
                     completion(.failure(error))
@@ -151,11 +175,15 @@ public struct Cardservice {
     /// Fetch all transactions that occurred in a specific time interval.
     /// - Parameters:
     ///   - begin: start of time interval
-    ///   - end: end of time interval
+    ///   - end: end of time interval, defaults to now
     ///   - session: URLSession, defaults to .shared
     /// - Returns: A list of transactions
     @available(macOS 12.0, iOS 15.0, *)
-    public func transactions(begin: Date, end: Date, session: URLSession = .shared) async throws -> [Transaction] {
+    public func transactions(
+        begin: Date,
+        end: Date = Date(),
+        session: URLSession = .shared
+    ) async throws -> [Transaction] {
         try await withCheckedThrowingContinuation { continuation in
             self.transactions(begin: begin, end: end, session: session) { result in
                 continuation.resume(with: result)
